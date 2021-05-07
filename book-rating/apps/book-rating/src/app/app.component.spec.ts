17cea5a956328c96aa1d99dfb5e3bf557fe24686
Ferdinand Malcher <ferdinand@malcher.media>
2021-05-07 16:23:43 +0200

    AppComponent geleert

--- book-rating/apps/book-rating/src/app/app.component.spec.ts
+++ book-rating/apps/book-rating/src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'book-rating'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('book-rating');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to book-rating!'
-    );
-  });
 });
