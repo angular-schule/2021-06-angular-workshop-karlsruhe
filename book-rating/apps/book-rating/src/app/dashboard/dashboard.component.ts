1e46236d2eb98aa47998a375888422c7c71694c4
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-06-24 14:30:33 +0200

    nx g lib feature-books --routing --styles=scss --prefix=books

--- book-rating/apps/book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/apps/book-rating/src/app/dashboard/dashboard.component.ts
@@ -7,20 +7,24 @@ import { Book } from '../shared/book';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent {
-  books: Book[] = [{
-    isbn: '001',
-    title: 'Angular',
-    description: 'Tolles Buch',
-    rating: 5
-  }, {
-    isbn: '002',
-    title: 'React',
-    description: 'Ok Buch',
-    rating: 3
-  }, {
-    isbn: '003',
-    title: 'jQUery',
-    description: 'Altes Buch',
-    rating: 1
-  }];
+  books: Book[] = [
+    {
+      isbn: '001',
+      title: 'Angular',
+      description: 'Tolles Buch',
+      rating: 5,
+    },
+    {
+      isbn: '002',
+      title: 'React',
+      description: 'Ok Buch',
+      rating: 3,
+    },
+    {
+      isbn: '003',
+      title: 'jQUery',
+      description: 'Altes Buch',
+      rating: 1,
+    },
+  ];
 }
--- book-rating/jest.config.js
+++ book-rating/jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  projects: ['<rootDir>/apps/book-rating'],
+  projects: ['<rootDir>/apps/book-rating', '<rootDir>/libs/feature-books'],
 };
