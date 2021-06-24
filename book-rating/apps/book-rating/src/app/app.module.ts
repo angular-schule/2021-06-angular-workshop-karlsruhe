8c4b23e1c275a89e9d44a5b1abbf3162296cd005
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-06-24 14:39:51 +0200

    nx: constraints

--- book-rating/apps/book-rating/src/app/app.module.ts
+++ book-rating/apps/book-rating/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FeatureBooksModule } from '@book-rating/feature-books'
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -13,6 +14,7 @@ const appRoutes: Routes = [];
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabled' }),
+    FeatureBooksModule
   ],
   providers: [],
   bootstrap: [AppComponent],
