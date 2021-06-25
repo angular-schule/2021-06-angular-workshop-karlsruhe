e84f8c987475aee958d945638210630542ac959f
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-06-25 11:13:38 +0200

    nx g lib data-books && nx g s book-rating --project=data-books

--- book-rating/jest.config.js
+++ book-rating/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/book-rating', '<rootDir>/libs/feature-books'],
+  projects: [
+    '<rootDir>/apps/book-rating',
+    '<rootDir>/libs/feature-books',
+    '<rootDir>/libs/data-books',
+  ],
 };
---
+++ book-rating/libs/data-books/README.md
@@ -0,0 +1,7 @@
+# data-books
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test data-books` to execute the unit tests.
