b2e0f9befe2c1ca33f36cfd8c93b256a5c0c18d9
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-06-25 13:18:33 +0200

    TDD: hands on!

--- book-rating/libs/data-books/src/lib/book-rating.service.spec.ts
+++ book-rating/libs/data-books/src/lib/book-rating.service.spec.ts
@@ -1,16 +1,47 @@
-import { TestBed } from '@angular/core/testing';
-
+import { Book } from './book';
 import { BookRatingService } from './book-rating.service';
 
 describe('BookRatingService', () => {
   let service: BookRatingService;
+  let book: Book;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BookRatingService);
+    service = new BookRatingService();
+    book = {
+      isbn: '',
+      title: '',
+      rating: 3,
+      description: ''
+    };
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should rate up a book by one', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(4);
   });
+
+  it('should rate down a book by one', () => {
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(2);
+  });
+
+  it('should not be allowed to have a rating greater than 5', () => {
+    book.rating = 5;
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook.rating).toBe(5);
+  });
+
+  it('should not be allowed to have a rating smaller than 1', () => {
+    book.rating = 1;
+    const ratedBook = service.rateDown(book);
+    expect(ratedBook.rating).toBe(1);
+  });
+
+  it('it should respect immutability and return a new book', () => {
+    const ratedBook = service.rateUp(book);
+    expect(ratedBook).not.toBe(book);
+
+    const ratedBook2 = service.rateDown(book);
+    expect(ratedBook2).not.toBe(book);
+  })
 });
