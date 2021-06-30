db06ecb5ab5a72ae56c3d172efac5200468c0d4d
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-06-30 14:27:56 +0200

    ng add @ngrx/store ng add @ngrx/effects ng add @ngrx/store-devtools

--- book-rating/apps/book-rating/src/app/app.module.ts
+++ book-rating/apps/book-rating/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { API_URL } from '@book-rating/data-books';
 import { environment } from '../environments/environment';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 // import { DashboardComponent } from './dashboard/dashboard.component';
 // import { BookComponent } from './book/book.component';
 
@@ -26,7 +29,10 @@ const appRoutes: Routes = [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabled' }),
     // FeatureBooksModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [{
     provide: API_URL,
