ce078df4b017ac66913f5cc6fa106f1f9dafda6c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-06-30 14:30:49 +0200

    ng g @ngrx/schematics:feature store/book --module data-books --api --project=data-books --no-interactive

--- book-rating/libs/data-books/src/lib/data-books.module.ts
+++ book-rating/libs/data-books/src/lib/data-books.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import * as fromBook from './store/book.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './store/book.effects';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, StoreModule.forFeature(fromBook.bookFeatureKey, fromBook.reducer), EffectsModule.forFeature([BookEffects])],
 })
 export class DataBooksModule {}
